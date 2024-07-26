0ee6f554a214e762a8b21dc78f287652f428db8b
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-07-26 18:19:17 +0530

    causes page added

--- health_web/src/pages/CausesofDepression.jsx
+++ health_web/src/pages/CausesofDepression.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import causimg from "../assets/mental health4.png"
 
 export default function CausesofDepression() {
+
+  const causes_list =["Early childhood trauma.","Severe illness e.g. heart disease.","Drug abuse.","Family history."];
   return (
     <div>
+      <h1>Causes of depression</h1>
+      <div>
+        <img src={causimg}/>
+        <div>
+          <ol>
+          {causes_list.map((cause,index)=>(
+              <li key={index}>{cause}</li>
+            ))}
+          </ol>
+          
+
+        </div>
+      </div>
       
     </div>
   )
--- health_web/src/pages/SignsandSymptoms.jsx
+++ health_web/src/pages/SignsandSymptoms.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import signsimg from "../assets/mental health 3.png"
 
 export default function SignsandSymptoms() {
+  const sign_list =["Persistent sadness","Feelings of guilt and hopelessness.","Loss of interest in activities one enjoyed e.g. hobbies.","Fatigue","Sudden loss or gain of appetite."];
   return (
     <div>
-      <h1>Sri Lanka</h1>
+      <h1>Signs and symptoms of depression</h1>
+      <div>
+        <img src={signsimg}></img>
+        <div>
+          <ol style={{listStyle:"upper-roman"}}>
+            {sign_list.map((sign,index)=>(
+              <li key={index}>{sign}</li>
+            ))}
+          </ol>
+
+        </div>
+      </div>
     </div>
   )
 }
