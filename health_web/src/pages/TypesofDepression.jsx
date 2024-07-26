c5194ec9ec872786dfd95dad9bad4f5821b121ea
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-07-26 17:09:23 +0530

    types of depression added

--- health_web/src/pages/TypesofDepression.jsx
+++ health_web/src/pages/TypesofDepression.jsx
@@ -3,6 +3,22 @@ import React from 'react'
 export default function TypesofDepression() {
   return (
     <div>
+      <h1>Types of Depression</h1>
+
+      <div>
+        <p>01</p>
+        <div>
+          <p>Major Depression</p>
+          <p>A type of depression where symptoms occur nearly every day for at least two weeks.</p>
+        </div>
+      </div>
+      <div>
+        <p>02</p>
+        <div>
+          <p>Persistent depression</p>
+          <p>A type of depression where symptoms occur for at least two years.</p>
+        </div>
+      </div>
       
     </div>
   )
