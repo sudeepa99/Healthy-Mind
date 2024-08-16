55c36c959b0fd55a6a047f5825fcea7e8e7c83a9
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-08-17 01:09:23 +0530

    Worked in homepage

--- health_web/src/components/Navigation.jsx
+++ health_web/src/components/Navigation.jsx
@@ -5,20 +5,24 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function Navigation() {
+  const dropDownStyle={
+    color:"#e0f2fe",
+    fontweight: 'bold'
+  };
   return (
-    <div>
-        <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Nav className="me-auto">
-            <NavDropdown title="Types of depression" id="types">
-              <NavDropdown.Item href="#major">Major depression</NavDropdown.Item>
-              <NavDropdown.Item href="#persistent">Persistent depression</NavDropdown.Item>
+    <div className='flex-row gap-16 bg-custom-blue'  >
+        <Navbar>
+        <Container >
+          <Nav >
+            <NavDropdown title={<span style={dropDownStyle}>Types of Depression </span>} id="types" className='text-white'>
+              <NavDropdown.Item href="#major" className='text-black'>Major depression</NavDropdown.Item>
+              <NavDropdown.Item href="#persistent" className='text-black'>Persistent depression</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#types"></Nav.Link>
-            <Nav.Link href="#signs">Signs and symptoms</Nav.Link>
-            <Nav.Link href="#causes">Causes of depression</Nav.Link>
-            <Nav.Link href="#treatment">Treatment methods</Nav.Link>
-            <Nav.Link href='#handling'>Handling depression</Nav.Link>
+            <Nav.Link href="#types" className='text-cyan-50'></Nav.Link>
+            <Nav.Link href="#signs" className='text-cyan-50'>Signs and symptoms</Nav.Link>
+            <Nav.Link href="#causes" className='text-cyan-50'>Causes of depression</Nav.Link>
+            <Nav.Link href="#treatment" className='text-cyan-50'>Treatment methods</Nav.Link>
+            <Nav.Link href='#handling' className='text-cyan-50'>Handling depression</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
