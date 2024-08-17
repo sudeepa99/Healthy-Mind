cd738af559a805572c1b77f86bc72c4b38c26955
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-08-17 19:44:09 +0530

    Sign syptoms finalized

--- health_web/src/pages/SignsandSymptoms.jsx
+++ health_web/src/pages/SignsandSymptoms.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import signsimg from "../assets/mental health 3.png"
+import Navigation from '../components/Navigation';
 
 export default function SignsandSymptoms() {
   const sign_list =["Persistent sadness","Feelings of guilt and hopelessness.","Loss of interest in activities one enjoyed e.g. hobbies.","Fatigue","Sudden loss or gain of appetite."];
   return (
-    <div>
-      <h1>Signs and symptoms of depression</h1>
-      <div>
-        <img src={signsimg}></img>
-        <div>
-          <ol style={{listStyle:"upper-roman"}}>
+    <div className='w-screen h-screen bg-Background-blue'>
+      <Navigation/>
+      <div className='flex flex-col mt-14'>
+        <h1 className='text-5xl tracking-wide text-center text-white '>Signs and symptoms of depression</h1>
+        <div className='flex flex-row content-center justify-center mt-16 gap-14'>
+          <img className='opacity-55' src={signsimg}></img>
+          <div className='pt-6 text-xl leading-10 tracking-wide text-white opacity-45'>
+            <ol  style={{listStyle:"upper-roman"}}>
             {sign_list.map((sign,index)=>(
               <li key={index}>{sign}</li>
             ))}
-          </ol>
-
+            </ol>
+          </div>
         </div>
+ 
       </div>
+      
     </div>
   )
 }
